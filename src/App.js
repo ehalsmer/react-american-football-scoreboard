a262a38b43b1578ecb966fb4c28830cab8ef9ca6
Eleasah Halsmer <eleasahjh@gmail.com>
2019-07-15 15:52:39 -0600

    Added clickHandle function, which accepts team and amount arguments. Added button to reset score of both teams to 0

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ function App() {
   //TODO: STEP 2 - Establish your application's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
+
+  function clickHandle(team, amount){
+    if (team === 'home'){
+      setHomeScore(homeScore + amount);
+    } else if (team === 'away'){
+      setAwayScore(awayScore + amount);
+    }
+  }
+
   return (
     <div className="container">
       <section className="scoreboard">
@@ -29,12 +38,15 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={()=>setHomeScore(homeScore + 7)}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={()=>setHomeScore(homeScore + 3)}>Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick={()=>clickHandle('home', 7)}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick={()=>clickHandle('home', 3)}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={()=>setAwayScore(awayScore + 7)}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={()=>setAwayScore(awayScore + 3)}>Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={()=>clickHandle('away', 7)}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick={()=>clickHandle('away', 3)}>Away Field Goal</button>
+        </div>
+        <div>
+          <button className="resetButton" onClick={()=>{setHomeScore(0); setAwayScore(0)}}>Reset Score</button>
         </div>
       </section>
     </div>
